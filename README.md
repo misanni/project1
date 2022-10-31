dd736a9f55b5d1a8f5828cf1db36738b85e9c4d9
Mary Sanni <marysanni01@gmail.com>
2022-10-30 23:44:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Name of App: News search App
+### Created by Mary Sanni.
+
+A description of my project: This is to enable users to search for any news of their choice
+
+### Technologies Used
+
+- HTML
+- CSS
+- JAVASCRIPT
+- API
+
+### Wireframes
+(https://imgur.com/a/HXLscoy)
+
+### Daily Plan
+
+| Day | Goal |
+|-----|------|
+| 1 | Choose API |
+| 2 | Design the user interface (html, css)|
+| 3 | Generate Result from API (javaScript)|
+|4| Test App (Google Chrome)
+
+
+### Link to Project
+[My Project Live Page](https://github.com/misanni/project1)
---
+++ script.js
@@ -0,0 +1,36 @@
+
+    //const addEventListner =document.queryselector('submit');
+    const searchFrom= document.querySelector('.search');
+    const input=document.querySelector('.input');
+    const newsList=document.querySelector('.news-list');
+    console.log(newsList);
+  searchFrom.addEventListener('submit', retrieve)
+    function retrieve(e){
+        if (input.value== ''){
+        alert('input field is empty!')
+        return
+    }
+        newsList.innerHTML=''
+        e.preventDefault()
+    const apikey = '241e074b3c3945cf850407a246c8b648' 
+    let topic = input.value;
+    let url=`https://newsapi.org/v2/everything?q=${topic}&apiKey=${apikey}`
+   
+    fetch(url).then((res)=>{
+        return res.json()
+    }).then((data)=>{
+        console.log(data)
+        data.articles.forEach(article=>{
+            let li=document.createElement('li');
+            let a = document.createElement('a');
+            a.setAttribute('href', article.url );
+            a.setAttribute('target', '_blank');
+            a.textContent=article.title;
+            li.appendChild(a);
+            newsList.appendChild(li);
+        })
+    }).catch((error)=>{
+        console.log(error)
+    })
+   
+    }
